2286306e93f9b5314b64f10afbccbf2e18e1673a
Robert Jackson <rjackson@linkedin.com>
2020-04-05 11:31:24 -0400

    Remove `any` usage from main entry-point.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { traverse, builders, Walker } from '@glimmer/syntax';
+import type { AST, NodeVisitor } from '@glimmer/syntax';
 import ParseResult from './parse-result';
 
 const PARSE_RESULT_FOR = new WeakMap();
 
-export function parse(template: string) {
+export function parse(template: string): AST.Template {
   const result = new ParseResult(template);
 
   PARSE_RESULT_FOR.set(result.ast, result);
@@ -11,12 +12,41 @@ export function parse(template: string) {
   return result.ast;
 }
 
-export function print(ast: any) {
+export function print(ast: AST.Node): string {
   const parseResult = PARSE_RESULT_FOR.get(ast);
   return parseResult.print();
 }
 
-export function transform(template: string, plugin: any) {
+export interface Syntax {
+  parse: typeof parse;
+  builders: typeof builders;
+  print: typeof print;
+  traverse: typeof traverse;
+  Walker: typeof Walker;
+}
+
+export interface TransformPluginEnv {
+  syntax: Syntax;
+}
+
+export interface TransformPluginBuilder {
+  (env: TransformPluginEnv): NodeVisitor;
+}
+
+export interface ASTPlugin {
+  name: string;
+  visitor: NodeVisitor;
+}
+
+export interface TransformResult {
+  ast: AST.Node;
+  code: string;
+}
+
+export function transform(
+  template: string | AST.Template,
+  plugin: TransformPluginBuilder
+): TransformResult {
   let ast;
   if (typeof template === 'string') {
     ast = parse(template);
